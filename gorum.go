38be847e5c104f3c55f8a3418bc0051c9a70a438
Chuck Preslar <chuckpreslar@gmail.com>
2013-10-04 13:36:06 -0400

    gofmt formatting.

--- gorum.go
+++ gorum.go
@@ -3,16 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path"
 )
 
 import (
 	"github.com/jroes/gorum/models"
 )
 
+const (
+	UserStoreDirectory = "users"
+)
+
+var (
+	directory string
+)
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	var userStore models.UserStore
-	userStore = models.NewUserGobStore("users/")
+	userStore = models.NewUserGobStore(path.Join(directory, UserStoreDirectory))
+
 	user, err := userStore.FindUser("jon@example.com")
+
 	if err != nil {
 		fmt.Fprintf(w, "Had trouble finding jon@example.com, attempting creation...\n")
 		user = models.NewUser("jon@example.com", "password1!")
@@ -24,16 +36,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Hello world, %v!\n", user)
+
 	err = user.HasPassword(r.URL.Path[1:])
+
 	if err != nil {
 		fmt.Fprintf(w, "Looks like you have the wrong password!\n")
 		return
 	}
+
 	fmt.Fprintf(w, "Looks like you have the matching password!\n")
 }
 
 func main() {
 	fmt.Println("Starting server...")
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	if directory, err := os.Getwd(); nil != err {
+		panic(err)
+	} else if err = http.ListenAndServe(":8080", nil); nil != err {
+		panic(err)
+	}
 }
